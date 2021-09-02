07db155038a97cb08e64b848c224f829f44ef05d
Crowdbotics <team@crowdbotics.com>
2021-09-02 10:36:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "klicksaya_30270Identifier",
+  url: "https://klicksaya_30270Identifier.botics.co",
+  api: "https://klicksaya_30270Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
